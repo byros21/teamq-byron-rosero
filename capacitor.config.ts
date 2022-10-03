a44817e5aaed86db4349626ce3b67807086682db
byros21 <byros3@yahoo.com>
2022-10-03 12:12:24 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-teamq',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
